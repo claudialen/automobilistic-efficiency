bb76a65254a782a2ad100f89e020bc6fba56194a
claudialen <83750140+claudialen@users.noreply.github.com>
2022-11-10 19:05:22 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main(){
-  
+ofstream out;
+
+void sortida(string output, ...)
+{
+    ofstream out(output);
+    out << ... << endl;
+    out << ... << endl;
+    out.close();
+}
+
+int main(int argc, char **argv)
+{
+    string input = string(argv[1]);
+    ifstream f(input);
+    f >> ;
+    f.close();
 }
