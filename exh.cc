5d49c75616dbfec0b7010b3daa96abb093b02de0
claudialen <83750140+claudialen@users.noreply.github.com>
2022-11-10 19:40:51 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -6,18 +6,40 @@ using namespace std;
 
 ofstream out;
 
-void sortida(string output, ...)
+void sortida(string output, int inici, int penalitzacio, ...)
 {
     ofstream out(output);
-    out << ... << endl;
-    out << ... << endl;
+    double temps = (clock() - inici) / (double)CLOCKS_PER_SEC;
+    out << penalitzacio << ' ' << temps << endl;
     out.close();
 }
 
 int main(int argc, char **argv)
 {
+    int inici = clock();
     string input = string(argv[1]);
     ifstream f(input);
-    f >> ;
+    int C, M, K;
+    f >> C >> M >> K;
+    for (int i = 0; i < M; i++)
+    {
+        // ns si haurem de fer un vector o crear una struct d millores (crec q vector)
+        f >> ce;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        // lo mismo q amb ce
+        f >> ne;
+    }
+    for (int i = 0; i < K; i++)
+    {
+        // lo mismo q amb ce
+        f >> id >> cotxes;
+        for (int i = 0; i < M; i++)
+        {
+            f >> millora;
+        }
+    }
+
     f.close();
 }
