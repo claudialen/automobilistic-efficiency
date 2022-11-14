a1c83f9c00ab84c0be5997eab6d5491a14a2020b
alinacastell <105283569+alinacastell@users.noreply.github.com>
2022-11-14 18:08:36 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -15,7 +15,7 @@ using VVB = vector<VB>;
 VI ce, ne;
 VVI linia;
 VVB estacions;
-int penalitzacio;
+int penalitzacio_act;
 
 void sortida(string output, int inici, ...)
 {
@@ -25,22 +25,36 @@ void sortida(string output, int inici, ...)
     out.close();
 }
 
-void backtrack(int& k, vector<int>& solparcial)
+void backtrack(int& cotxes_constr, vector<int>& produccio, vector<int>& solparcial, vector<int>& millores, int& penalitzacio_max)
 {
-    int M = ce.size();
-    int id = solparcial[k];
+    if (penalitzacio_act > penalitzacio_max)
+        return;
+    if (cotxes_constr == solparcial.size()) {
+        penalitzacio_max = penalitzacio_act;
+        sortida();
+    } // sortida()
     // per a cada estacio afegir si la classe te aquella millora
-    for (int m = 0; m < M; ++m) {
-        // fila: id de la classe, columna: les M millores
-        if (linia[id][m] == 1) {
-            estacions[m][k] = true;
+    else {
+        int M = ce.size();
+        for (int m = 0; m < M; ++m) {
+            // aixo esta malament, el vector no va be
+            if (estacions[klasse][m])
+                ++millores[m];
+            if (millores[m] >= ne[m]) {
+                // aquest cotxes_constr hauria de ser cotxes consecutius
+                penalitzacio_act += max(cotxes_constr - ce[m], 0);
+            }
         }
-        if (k + 1 >= ne[m]) {
-            int cotxes_cons = 0;
-            for (int j = k + 1 - ne[m]; j < k + 1; ++j)
-                if (estacions[m][j])
-                    ++cotxes_cons;
-            penalitzacio += max(cotxes_cons - ce[m], 0);
+        // classe es el punter que recorre totes les classes
+        for (int classe = 0; classe < produccio.size(); classe++) {
+            if (penalitzacio_act > penalitzacio_max)
+                backtrack(cotxes_constr, produccio, solparcial, millores, penalitzacio_max);
+            else if (produccio[classe] > 0) {
+                produccio[classe]--;
+                solparcial[cotxes_constr] = classe;
+                backtrack(++cotxes_constr, produccio, solparcial, millores, penalitzacio_max);
+                produccio[classe]++;
+            }
         }
     }
 }
@@ -53,7 +67,7 @@ int main(int argc, char** argv)
 
     int C, M, K;
     f >> C >> M >> K;
-    // creacio de vectors de millores, de la linia de produccio i de cada estacio
+    // creacio vectors de millores, de la linia de produccio i de cada estacio
     ce = VI(M);
     ne = VI(M);
     linia = VVI(K, VI(M + 2, -1));
@@ -70,10 +84,10 @@ int main(int argc, char** argv)
     }
     for (int i = 0; i < K; i++) {
         // identificador i nombre de cotxes de la classe
-        f >> linia[i][0] >> linia[i][1];
+        f >> classe >> produccio[classe];
         for (int j = 2; j < M + 2; j++) {
             // millores requerides per la classe
-            f >> linia[i][j];
+            f >> estacions[i][j];
         }
     }
 
