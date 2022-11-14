c2924c145f56c148e0c593d8b7774d4820d8d34c
alinacastell <105283569+alinacastell@users.noreply.github.com>
2022-11-14 18:38:51 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -12,39 +12,41 @@ using VVI = vector<VI>;
 using VB = vector<bool>;
 using VVB = vector<VB>;
 
-VI ce, ne;
-VVI linia;
+VI ce, ne, produccio;
 VVB estacions;
 int penalitzacio_act;
+int classe;
 
 void sortida(string output, int inici, ...)
 {
     ofstream out(output);
     double temps = (clock() - inici) / (double)CLOCKS_PER_SEC;
-    out << penalitzacio << ' ' << temps << endl;
+    out << penalitzacio_act << ' ' << temps << endl;
     out.close();
 }
 
-void backtrack(int& cotxes_constr, vector<int>& produccio, vector<int>& solparcial, vector<int>& millores, int& penalitzacio_max)
+void backtrack(int& cotxes_constr, vector<int>& produccio,
+    vector<int>& solparcial, vector<int>& millores,
+    int& penalitzacio_max)
 {
     if (penalitzacio_act > penalitzacio_max)
         return;
     if (cotxes_constr == solparcial.size()) {
         penalitzacio_max = penalitzacio_act;
-        sortida();
-    } // sortida()
+        //sortida();
+    }
     // per a cada estacio afegir si la classe te aquella millora
     else {
         int M = ce.size();
-        for (int m = 0; m < M; ++m) {
-            // aixo esta malament, el vector no va be
+        /*for (int m = 0; m < M; ++m) {
+            // aixo esta malament
             if (estacions[klasse][m])
                 ++millores[m];
             if (millores[m] >= ne[m]) {
                 // aquest cotxes_constr hauria de ser cotxes consecutius
                 penalitzacio_act += max(cotxes_constr - ce[m], 0);
             }
-        }
+        }*/
         // classe es el punter que recorre totes les classes
         for (int classe = 0; classe < produccio.size(); classe++) {
             if (penalitzacio_act > penalitzacio_max)
@@ -70,7 +72,7 @@ int main(int argc, char** argv)
     // creacio vectors de millores, de la linia de produccio i de cada estacio
     ce = VI(M);
     ne = VI(M);
-    linia = VVI(K, VI(M + 2, -1));
+    produccio = VI(K);
     estacions = VVB(M, VB(C, false));
 
     // inicialitzacio d'estructures
@@ -86,8 +88,13 @@ int main(int argc, char** argv)
         // identificador i nombre de cotxes de la classe
         f >> classe >> produccio[classe];
         for (int j = 2; j < M + 2; j++) {
+            int aplica_millora;
             // millores requerides per la classe
-            f >> estacions[i][j];
+            f >> aplica_millora;
+            if (aplica_millora)
+                estacions[i][j] = true;
+            else
+                estacions[i][j] = false;
         }
     }
 
