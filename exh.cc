3fbe5f8570be1b652e6f65f491c6a242646f7fd9
claudialen <83750140+claudialen@users.noreply.github.com>
2022-12-28 17:35:32 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -14,19 +14,21 @@ using VVI = vector<VI>;
 using VB = vector<bool>;
 using VVB = vector<VB>;
 
-int MAX_VAL = 1000000;
+string input, output;
+int inici, pen_max = INT_MAX, C, M, K;
+VI solucio, ne, ce, produccio;
+VVB estacions;
 
 /*
 Funció que escriu la solució final sobre el fitxer sortida.
 Variables requerides: nom del fitxer sortda (output), algoritme d'inici (inici),
 penalitzacio final (pen_max) i la solució final (solucio).
 */
-void sortida(string output, int inici, int pen_max, const VI& solucio)
+void sortida()
 {
     ofstream out(output); // Creates the output file
     // Calculates the time the algorithm spent finding the last solution
-    double temps = (clock() - inici) / (double)CLOCKS_PER_SEC;
-    out << pen_max << ' ' << temps << endl; // Writes the penalty
+    out << pen_max << ' ' << (clock() - inici) / (double)CLOCKS_PER_SEC << endl; // Writes the penalty
     for (auto x : solucio)
         out << x << " "; // Writes the solution on the file
     out << endl;
@@ -38,14 +40,16 @@ Funció que crea intervals de mida ne.
 Paràmetres: inici i final de l'interval (a,b), la millora tractada (m), solució
 parcial i vector de nombre màxim de millores ne.
 */
-VI setinterval(int a, int b, int m, const VI& solparcial, const VI& ne)
+VI setinterval(int a, int b, const VI &solparcial)
 {
-    if (a < 0) {
+    if (a < 0)
+    {
         // Si la posició inicial és menor que 0, redefinim x=0
         a = 0;
     }
     VI interval;
-    while (a < b) {
+    while (a < b)
+    {
         interval.push_back(solparcial[a]);
         ++a;
     }
@@ -53,43 +57,50 @@ VI setinterval(int a, int b, int m, const VI& solparcial, const VI& ne)
 }
 
 /* Funció que calcula el nombre de penalitzacions. */
-int penalitzacions(int cotxes, const VI& solparcial, const VVB& estacions,
-    const VI& ne, const VI& ce)
+int penalitzacions(int cotxes, const VI &solparcial)
 {
     // Nombre de penalitzacions per afegir un nou cotxe a la solparcial
     int pen = 0;
-    int M = ne.size();
-    int C = solparcial.size();
-
     // Vector de classes a comptar penalitzacions de la solucio solparcial
     VI interval;
     // Per cada millora m recorrem totes les seves classes k
-    for (int m = 0; m < M; m++) {
+    for (int m = 0; m < M; m++)
+    {
         int cotxes_millora = 0;
-        if (cotxes == C) {
-            for (int i = cotxes - ne[m]; i < cotxes; i++) {
+        if (cotxes == C)
+        {
+            for (int i = cotxes - ne[m]; i < cotxes; i++)
+            {
                 cotxes_millora = 0;
                 // Mirem si l'interval ne té penalitzacions
-                interval = setinterval(i, cotxes, m, solparcial, ne);
-                for (int k = 0; k < int(interval.size()); k++) {
-                    if (estacions[interval[k]][m]) {
+                interval = setinterval(i, cotxes, solparcial);
+                for (int k = 0; k < int(interval.size()); k++)
+                {
+                    if (estacions[interval[k]][m])
+                    {
                         cotxes_millora++;
                     }
                 }
 
                 // Nombre de cotxes consecutius és major que el màxim permès
-                if (cotxes_millora > ce[m]) {
+                if (cotxes_millora > ce[m])
+                {
                     pen += cotxes_millora - ce[m];
                 }
             }
-        } else {
-            interval = setinterval(cotxes - ne[m], cotxes, m, solparcial, ne);
-            for (int k = 0; k < int(interval.size()); k++) {
-                if (estacions[interval[k]][m]) {
+        }
+        else
+        {
+            interval = setinterval(cotxes - ne[m], cotxes, solparcial);
+            for (int k = 0; k < int(interval.size()); k++)
+            {
+                if (estacions[interval[k]][m])
+                {
                     cotxes_millora++;
                 }
             }
-            if (cotxes_millora > ce[m]) {
+            if (cotxes_millora > ce[m])
+            {
                 pen += cotxes_millora - ce[m];
             }
         }
@@ -97,72 +108,71 @@ int penalitzacions(int cotxes, const VI& solparcial, const VVB& estacions,
     return pen;
 }
 
-void backtrack(int cotxes, VI& solparcial, VI& solucio, int pen_act,
-    int& pen_max, VI& produccio, const VI& ne, const VI& ce,
-    const VVB& estacions, const string output, const int inici)
+void backtrack(int cotxes, VI &solparcial, int pen_act)
 {
-    int K = produccio.size();
-    int C = solparcial.size();
     // Si la penalització de la solució actual és major que la màxima no seguim
-    if (pen_act >= pen_max) {
+    if (pen_act >= pen_max)
+    {
         return;
     }
     // Si el nombre de cotxes construits equival al total a construir
-    if (cotxes == C) {
-        if (pen_act < pen_max) {
+    if (cotxes == C)
+    {
+        if (pen_act < pen_max)
+        {
             pen_max = pen_act;
             solucio = solparcial;
-            sortida(output, inici, pen_max, solucio);
+            sortida();
         }
     }
     // Per a cada estacio afegir si la classe k te aquella millora
-    else {
+    else
+    {
         // k és el punter que recorre totes les classes
-        for (int k = 0; k < K; k++) {
+        for (int k = 0; k < K; k++)
+        {
             // Si encara queden cotxes a construir d'aquella classe k
-            if (produccio[k] > 0) {
+            if (produccio[k] > 0)
+            {
                 solparcial[cotxes] = k;
-                pen_act += penalitzacions(cotxes + 1, solparcial, estacions, ne, ce);
+                pen_act += penalitzacions(cotxes + 1, solparcial);
                 --produccio[k];
-                backtrack(cotxes + 1, solparcial, solucio, pen_act, pen_max,
-                    produccio, ne, ce, estacions, output, inici);
+                backtrack(cotxes + 1, solparcial, pen_act);
                 ++produccio[k];
-                pen_act -= penalitzacions(cotxes + 1, solparcial, estacions, ne, ce);
+                pen_act -= penalitzacions(cotxes + 1, solparcial);
             }
         }
     }
 }
 
-int main(int argc, char** argv)
+void llegir_dades()
 {
-    // Es llegeixen input de fitxers
-    int inici = clock();
-    string input = string(argv[1]);
-    string output = string(argv[2]);
     ifstream f(input);
-
-    int C, M, K;
     f >> C >> M >> K;
     // Creacio vectors de millores, de la linia de produccio i de cada estacio
-    VI ce = VI(M);
-    VI ne = VI(M);
-    VI produccio = VI(K);
-    VVB estacions = VVB(K, VB(M, false));
+    ce.resize(M);
+    ne.resize(M);
+    produccio.resize(K);
+    estacions.resize(K, VB(M, false));
 
     // Inicialitzacio d'estructures
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++)
+    {
         // Capacitat de l'estacio
         f >> ce[i];
     }
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++)
+    {
         // Conjunt de cotxes consecutius maxim de cada estacio
         f >> ne[i];
     }
-    for (int i = 0; i < K; i++) {
+    for (int i = 0; i < K; i++)
+    {
         // Identificador i nombre de cotxes de cada classe k
         int classe;
         f >> classe >> produccio[classe];
-        for (int j = 0; j < M; j++) {
+        for (int j = 0; j < M; j++)
+        {
             int aplica_millora;
             // Millores requerides per la classe k
             f >> aplica_millora;
@@ -170,13 +180,20 @@ int main(int argc, char** argv)
                 estacions[i][j] = true;
         }
     }
+    f.close();
+}
+
+int main(int argc, char **argv)
+{
+    // Es llegeixen input de fitxers
+    inici = clock();
+    input = string(argv[1]);
+    output = string(argv[2]);
+    llegir_dades();
     // Es defineixen solucio parcial i final que utlitzara la funcio de backtracking
     VI solparcial(C);
-    VI solucio(C);
+    solucio.resize(C);
 
     // S'inicialitzen el nombre de cotxes construits i de penalitzacions a 0
-    int cotxes = 0, pen_act = 0, pen_max = MAX_VAL;
-    backtrack(cotxes, solparcial, solucio, pen_act, pen_max, produccio, ne, ce,
-        estacions, output, inici);
-    f.close();
+    backtrack(0, solparcial, 0);
 }
