819b5bc5af5a3a2ea899d9837353d7fd67871153
alinacastell <105283569+alinacastell@users.noreply.github.com>
2022-12-30 17:14:13 +0100

    Update exh.cc

--- exh.cc
+++ exh.cc
@@ -26,13 +26,12 @@ penalitzacio final (pen_max) i la solució final (solucio).
 */
 void sortida()
 {
-    ofstream out(output); // Creates the output file
-    // Calculates the time the algorithm spent finding the last solution
+    ofstream out(output);
     out << pen_max << ' ' << (clock() - inici) / (double)CLOCKS_PER_SEC << endl; // Writes the penalty
     for (auto x : solucio)
-        out << x << " "; // Writes the solution on the file
+        out << x << " ";
     out << endl;
-    out.close(); // Closes the file
+    out.close();
 }
 
 /*
@@ -40,100 +39,79 @@ Funció que crea intervals de mida ne.
 Paràmetres: inici i final de l'interval (a,b), la millora tractada (m), solució
 parcial i vector de nombre màxim de millores ne.
 */
-VI setinterval(int a, int b, const VI &solparcial)
+VI setinterval(int a, int b, const VI& solparcial)
 {
-    if (a < 0)
-    {
-        // Si la posició inicial és menor que 0, redefinim x=0
+    if (a < 0) {
+        // En cas que el valor entrat sigui menor a 0 (intervals incomplets)
         a = 0;
     }
     VI interval;
-    while (a < b)
-    {
+    while (a < b) {
         interval.push_back(solparcial[a]);
         ++a;
     }
     return interval;
 }
 
-/* Funció que calcula el nombre de penalitzacions. */
-int penalitzacions(int cotxes, const VI &solparcial)
+/*
+Funció que calcula el nombre de penalitzacions.
+*/
+int penalitzacions(int cotxes, const VI& solparcial)
 {
     // Nombre de penalitzacions per afegir un nou cotxe a la solparcial
     int pen = 0;
     // Vector de classes a comptar penalitzacions de la solucio solparcial
     VI interval;
     // Per cada millora m recorrem totes les seves classes k
-    for (int m = 0; m < M; m++)
-    {
+    for (int m = 0; m < M; m++) {
         int cotxes_millora = 0;
-        if (cotxes == C)
-        {
-            for (int i = cotxes - ne[m]; i < cotxes; i++)
-            {
+        if (cotxes == C) {
+            for (int i = cotxes - ne[m]; i < cotxes; i++) {
                 cotxes_millora = 0;
                 // Mirem si l'interval ne té penalitzacions
                 interval = setinterval(i, cotxes, solparcial);
-                for (int k = 0; k < int(interval.size()); k++)
-                {
+                for (int k = 0; k < int(interval.size()); k++) {
                     if (estacions[interval[k]][m])
-                    {
                         cotxes_millora++;
-                    }
                 }
 
                 // Nombre de cotxes consecutius és major que el màxim permès
                 if (cotxes_millora > ce[m])
-                {
                     pen += cotxes_millora - ce[m];
-                }
             }
-        }
-        else
-        {
+        } else {
             interval = setinterval(cotxes - ne[m], cotxes, solparcial);
-            for (int k = 0; k < int(interval.size()); k++)
-            {
+            for (int k = 0; k < int(interval.size()); k++) {
                 if (estacions[interval[k]][m])
-                {
                     cotxes_millora++;
-                }
             }
             if (cotxes_millora > ce[m])
-            {
                 pen += cotxes_millora - ce[m];
-            }
         }
     }
     return pen;
 }
 
-void backtrack(int cotxes, VI &solparcial, int pen_act)
+void backtrack(int cotxes, VI& solparcial, int pen_act)
 {
     // Si la penalització de la solució actual és major que la màxima no seguim
     if (pen_act >= pen_max)
-    {
         return;
-    }
+
     // Si el nombre de cotxes construits equival al total a construir
-    if (cotxes == C)
-    {
-        if (pen_act < pen_max)
-        {
+    if (cotxes == C) {
+        if (pen_act < pen_max) {
             pen_max = pen_act;
             solucio = solparcial;
             sortida();
         }
     }
     // Per a cada estacio afegir si la classe k te aquella millora
-    else
-    {
+    else {
         // k és el punter que recorre totes les classes
-        for (int k = 0; k < K; k++)
-        {
+        for (int k = 0; k < K; k++) {
             // Si encara queden cotxes a construir d'aquella classe k
-            if (produccio[k] > 0)
-            {
+            if (produccio[k] > 0) {
                 solparcial[cotxes] = k;
                 pen_act += penalitzacions(cotxes + 1, solparcial);
                 --produccio[k];
@@ -156,23 +134,19 @@ void llegir_dades()
     estacions.resize(K, VB(M, false));
 
     // Inicialitzacio d'estructures
-    for (int i = 0; i < M; i++)
-    {
+    for (int i = 0; i < M; i++) {
         // Capacitat de l'estacio
         f >> ce[i];
     }
-    for (int i = 0; i < M; i++)
-    {
+    for (int i = 0; i < M; i++) {
         // Conjunt de cotxes consecutius maxim de cada estacio
         f >> ne[i];
     }
-    for (int i = 0; i < K; i++)
-    {
+    for (int i = 0; i < K; i++) {
         // Identificador i nombre de cotxes de cada classe k
         int classe;
         f >> classe >> produccio[classe];
-        for (int j = 0; j < M; j++)
-        {
+        for (int j = 0; j < M; j++) {
             int aplica_millora;
             // Millores requerides per la classe k
             f >> aplica_millora;
@@ -183,7 +157,7 @@ void llegir_dades()
     f.close();
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     // Es llegeixen input de fitxers
     inici = clock();
